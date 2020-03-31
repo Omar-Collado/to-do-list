1fa2b2272c6b7507b34dd405f514a66c7577ceec
Omar Collado <omarcollado154@gmail.com>
2020-03-31 10:41:08 -0500

    Pushing latest changes from before

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ if(!localStorage.getItem("projects")) {
     projects.addProject("Schoolwork");
     let abc = todo("Do homework", "Math Class Problems 1-3", "10-20-1999", 0);
     projects.addTodo(0, abc);
-    console.log(projects.getProject(0));
-
 }
 
-pageRender.initalize(projects.allProjects);
+pageRender.initialize();
--- src/render.js
+++ src/render.js
@@ -1,13 +1,31 @@
+import {projects} from './projects';
 const pageRender = (() => {
     let sidebar = document.getElementsByClassName("sidebar")[0];
+    let main = document.getElementsByClassName("main")[0];
     const renderProjectTab = (project, id) => {
 
     }
-    const initialize = (projects) => {
-        for(let i = 0; i < projects.length; i++) {
+    const initialize = () => {
+        for(let i = 0; i < projects.allProjects.length; i++) {
             let p = document.createElement("p");
-            p.innerText = projects[i][0];
+            p.innerText = projects.allProjects[i][0];
             sidebar.appendChild(p);
+        }
+        renderTodos(projects.allProjects[0]);
+        
+    }
+    const renderTodos = (project) => {
+        console.log(project);
+        for(let i = 1; i < project.length; i++) {
+            let p = document.createElement("p");
+            let desc = document.createElement("p");
+            let due = document.createElement("p");
+            p.innerText = project[i].title;
+            desc.innerText = project[i].description;
+            due.innerText = project[i].dueDate;
+            main.appendChild(p);
+            main.appendChild(desc);
+            main.appendChild(due);
         }
     }
     return {initialize};
